cebd9620cf81f82a29eb0aa66c1fbf759bf789a2
fredydlemus <fflores@eaglehubs.com>
2023-10-25 16:33:21 -0600

    data migration completed

--- database/seeds/main.seeder.ts
+++ database/seeds/main.seeder.ts
@@ -4,36 +4,37 @@ import { Seeder, SeederFactoryManager } from "typeorm-extension";
 import { CategoryEntity } from "../../src/categories/category.entity";
 import { RecipeEntity } from "../../src/recipes/recipe.entity";
 import { TagEntity } from "../../src/tags/tag.entity";
-import { faker } from "@faker-js/faker";
 
 export default class MainSeeder implements Seeder {
     public async run(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<any> {
 
-        const userRepository = dataSource.getRepository(UserEntity);
-        const categoryRepository = dataSource.getRepository(CategoryEntity);
-        const tagRepository = dataSource.getRepository(TagEntity);
-        const recipeRepository = dataSource.getRepository(RecipeEntity);
+        const connection = dataSource.manager.connection;
+        await connection.transaction(async manager => {
 
-        const userFactory = factoryManager.get(UserEntity);
-        const categoryFactory = factoryManager.get(CategoryEntity);
-        const recipeFactory = factoryManager.get(RecipeEntity);
-        const tagFactory = factoryManager.get(TagEntity);
+            const userRepository = dataSource.getRepository(UserEntity);
+            const categoryRepository = dataSource.getRepository(CategoryEntity);
+            const tagRepository = dataSource.getRepository(TagEntity);
+            const recipeRepository = dataSource.getRepository(RecipeEntity);
 
-        await categoryFactory.saveMany(12);
-        await userFactory.saveMany(29);
-        await tagFactory.saveMany(40);
-        await recipeFactory.saveMany(5);
+            const userFactory = factoryManager.get(UserEntity);
+            const categoryFactory = factoryManager.get(CategoryEntity);
+            const recipeFactory = factoryManager.get(RecipeEntity);
+            const tagFactory = factoryManager.get(TagEntity);
 
-        const users = await userRepository.find();
-        const tags = await tagRepository.find();
-        const categories = await categoryRepository.find();
-        const recipes = await recipeRepository.find();
+            await categoryFactory.saveMany(12);
+            await userFactory.saveMany(29);
+            await tagFactory.saveMany(40);
+            await recipeFactory.saveMany(100);
 
-        await Promise.all(recipes.map(async (recipe) => {
-            recipe.user = faker.helpers.arrayElement(users);
-            recipe.category = faker.helpers.arrayElement(categories);
-            recipe.tags = faker.helpers.shuffle(tags).slice(0, 3);
-            await recipeRepository.save(recipe);
-        }));
+            const recipes = await recipeRepository.find();
+
+            await Promise.all(recipes.map(async (recipe) => {
+                recipe.user = await userRepository.createQueryBuilder().orderBy("RANDOM()").getOne();
+                recipe.category = await categoryRepository.createQueryBuilder().orderBy("RANDOM()").getOne();
+                recipe.tags = await tagRepository.createQueryBuilder().orderBy("RANDOM()").limit(3).getMany();
+            }));
+
+            await recipeRepository.save(recipes);
+        })
     }
 }
